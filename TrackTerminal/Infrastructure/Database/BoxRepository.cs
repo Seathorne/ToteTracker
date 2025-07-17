1fc926c4fe0dde596f89ccc7b28a4cc6980466bf
Seathorne <scottketclarke@gmail.com>
2025-07-17 18:32:40 -0400

    Add SQL server connection

---
+++ TrackTerminal/Infrastructure/Database/BoxRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace BoxSearch.Infrastructure.Database;
+
+internal class BoxRepository(string serverName)
+{
+    private readonly string _connectionString = @$"Server={serverName};Integrated Security=true;Encrypt=true;TrustServerCertificate=true";
+
+    //public async Task<SqlConnection> GetConnectionAsync()
+    //{
+    //    var connection = new SqlConnection(_connectionString);
+    //    await connection.OpenAsync();
+    //    return connection;
+    //}
+
+    public async Task TestConnectionAsync()
+    {
+        using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync();
+        Console.WriteLine("Successfully connected to SQL Server \"VARXACTATESTDB\"");
+        Console.WriteLine($"Server: {connection.DataSource}");
+        Console.WriteLine($"Database: {connection.Database}");
+    }
+}
--- TrackTerminal/Program.cs
+++ TrackTerminal/Program.cs
@@ -1,26 +1,58 @@
 ﻿using BoxSearch.Commands;
 using BoxSearch.Commands.Definitions;
+using BoxSearch.Infrastructure.Database;
+
 using LogParser;
 
 namespace BoxSearch;
 
 internal class Program
 {
-    private readonly static LogReader _logReader;
+    //private readonly static LogReader _logReader;
 
     static Program()
     {
-        _logReader = new() { TimeZone = TimeZoneInfo.Local };
-        _logReader.WriteBack += Console.WriteLine;
+        //_logReader = new() { TimeZone = TimeZoneInfo.Local };
+        //_logReader.WriteBack += Console.WriteLine;
     }
 
     public static async Task<int> Main(string[] args)
     {
         var registry = new CommandRegistry("boxsearch", "SanMar inventory box search tool");
         registry.Register(CreateProcessLogCommand());
+        registry.Register(CreateTestDatabaseCommand());
 
         CommandExecutor executor = new(registry);
         return await executor.ExecuteAsync(args);
+    }
+
+    private static CommandDefinition CreateTestDatabaseCommand()
+    {
+        var options = new List<OptionDefinition>
+        {
+            OptionDefinition.Create("server", "s", "Provide server name"),
+            OptionDefinition.Create("verbose", "v", "Enable verbose output")
+        };
+
+        return CommandDefinition.Create(name: "test-db", "Connect to database", options, async cmd =>
+        {
+            if (!cmd.Options.TryGetValue("server", out var optionValues) || optionValues.Count == 0)
+            {
+                Console.WriteLine("Error: must provide server name with --server");
+                return 1;
+            }
+            else if (optionValues.Count >= 2)
+            {
+                Console.WriteLine("Error: must specify a single server name");
+                return 1;
+            }
+
+            string serverName = optionValues[0];
+            BoxRepository repo = new(serverName);
+            await repo.TestConnectionAsync();
+
+            return 0;
+        });
     }
 
     private static CommandDefinition CreateProcessLogCommand()
@@ -40,7 +72,7 @@ internal class Program
             }
             else if (optionValues.Count >= 2)
             {
-                Console.WriteLine("Error: must specify a single file name with -i");
+                Console.WriteLine("Error: must specify a single file name");
                 return 1;
             }
 
@@ -49,9 +81,9 @@ internal class Program
             // TODO implement commands
             await Task.Run(() =>
             {
-                _logReader.CloseFile();
-                _logReader.OpenFile(fileName);
-                _logReader.ProcessFile();
+                //_logReader.CloseFile();
+                //_logReader.OpenFile(fileName);
+                //_logReader.ProcessFile();
             });
 
             return 0;
